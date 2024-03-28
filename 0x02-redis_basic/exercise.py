f8995dfde3eef51eab8bd8a38e6c3219f385473a
niialabi <taufiq.alabi@gmail.com>
2024-03-28 05:55:03 +0000

    added count method with decorators

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,20 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Any, Callable
 
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator to count the number of calls to a class method """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self: Any, *args: Any, **kwargs: Any) -> Any:
+        """ Wrapper function that increments a key in Redis for Cache.store """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 class Cache:
     """ Cache Class """
@@ -12,13 +24,15 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store method takes data argument and ret string """
         r_key = str(uuid.uuid1())
         self._redis.set(r_key, data)
         return r_key
 
-def get(self, key: str,
+    def get(self, key: str,
             fn: Callable = None) -> Union[str, bytes, int, float]:
         """ get data from cache using  key """
         if fn:
